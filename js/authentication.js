d45d2ee3305d24397089faf88fa79a7248bb1736
Justin Herrick <justin@justinherrick.com>
2014-11-19 12:24:27 -0500

    Moving app controller.

--- js/authentication.js
+++ js/authentication.js
@@ -1,3 +1,21 @@
+App.ApplicationController = Ember.Controller.extend({
+    // used to show, or not show, the log out button
+    isLoggedIn: false,
+    // when a user enters the app unauthenticated, the transition
+    // to where they are going is saved off so it can be retried
+    // when they have logged in.
+    savedTransition: null,
+
+    login: function() {
+      this.setProperties({ savedTransition: null, isLoggedIn: true });
+    },
+
+    logout: function() {
+      this.set('isLoggedIn', false);
+    }
+});
+
+
 App.ApplicationRoute = Ember.Route.extend({
   beforeModel: function(transition) {
     var applicationController = this.controllerFor('application');
--- js/router.js
+++ js/router.js
@@ -11,20 +11,3 @@ App.Router.map(function() {
 
     this.route('login', { path: '/login'});
 });
-
-App.ApplicationController = Ember.Controller.extend({
-    // used to show, or not show, the log out button
-    isLoggedIn: false,
-    // when a user enters the app unauthenticated, the transition
-    // to where they are going is saved off so it can be retried
-    // when they have logged in.
-    savedTransition: null,
-
-    login: function() {
-      this.setProperties({ savedTransition: null, isLoggedIn: true });
-    },
-
-    logout: function() {
-      this.set('isLoggedIn', false);
-    }
-});
